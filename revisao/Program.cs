9a01ebedcfd022461d78127182816eae46ca1446
icsalgado <icsalgado@icloud.com>
2021-01-17 21:48:03 -0300

    Criando menu de interface com usuario

---
+++ revisao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            
+            Console.WriteLine("__________________________");
+
+            string opcaoUsuario = Console.ReadLine();
+            
+            while(opcaoUsuario.ToUpper() != "X"){
+                switch(opcaoUsuario){
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break
+                    case "3":
+                        //TODO: calcular media geral
+                        break
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                Console.WriteLine("Informe a opção desejada");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Sair");
+
+                opcaoUsuario = Console.ReadLine();
+            }
+
+        }
+    }
+}
