c747e3977b0139dc215023ecb0e5cf8476b84171
icsalgado <icsalgado@icloud.com>
2021-01-20 01:30:48 -0300

    Conhecendo o Rntity Framework, criando aplicações asp.net BD

---
+++ curso-mvc/curso-mvc/Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace curso_mvc.Models
+{
+	public class Contexto : DbContext
+	{
+		public DbSet<Categoria> Categorias { get; set; }
+		public DbSet<Produto> Produtos { get; set; }		
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc; Integrated Security=True");
+		}
+	}
+}
